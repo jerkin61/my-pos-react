abcdb77d92709d0e02cdb379b73fde58bb6c2c50
Krisquin aka Jerquin <jerquinbayudo@gmail.com>
2022-03-17 14:06:51 +0800

    changes 3/17
    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
+import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
 import Query from "./components/Query/Query";
 
 const App = () => {
   return (
-  <><Header />
-  <Query /></> 
+  <><><Header />
+      <Query /></>
+      <Button /></>
   )
  
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Button.css'
+const Button = () => {
+  return (
+    <div className="frame-13 flex-col-hstart-vstart">
+      <div className="group-962 flex-row-vcenter">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/wfiw9bl26b-118%3A118?alt=media&token=562cce2d-0b5e-4ac4-b5c5-7c0149b92b6b"
+          alt="Not Found"
+          className="carbon-task-add"
+        />
+        <p className="txt-926">Add Product</p>
+      </div>
+    </div>
+  )
+}
+
+export default Button
