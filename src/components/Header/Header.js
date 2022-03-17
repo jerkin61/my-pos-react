d2e4ca512a9acaba31cf2229cecb6c3403eb03bf
Krisquin aka Jerquin <jerquinbayudo@gmail.com>
2022-03-17 13:58:36 +0800

    Changes 3/17
    Changes 3/17

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className="frame-2 flex-col-hstart-vstart">
+      <div className="frame-11 flex-col-hstart-vstart">
+        <div className="group-694">
+          <p className="txt-6100">Random Store</p>
+        </div>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/gn825ohxmek-118%3A95?alt=media&token=bef2b88f-8749-4c2e-89f9-9687ef1ae3da"
+        alt="Not Found"
+        className="frame-10"
+      />
+    </div>
+  )
+}
+
+export default Header
